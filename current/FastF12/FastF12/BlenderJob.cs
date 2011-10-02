e4f830a475e902f7f926a68b4259ea109eab2220
Shawn Wilkinson <super3boy@gmail.com>
2011-10-01 23:23:39 -0400

    complete abstract BlenderJob.cs

--- current/FastF12/FastF12/BlenderJob.cs
+++ current/FastF12/FastF12/BlenderJob.cs
@@ -13,22 +13,20 @@ namespace FastF12
     public class BlenderJob
     {
         // Basic Settings
-        private string[] ProjectName = "Untitled";
-        private RenderType RenderType = RenderType.Single;
-        private string sceneName = ""; 
+        private string ProjectName;
+        private RenderType RenderType;
+        private string sceneName;
 
-        private string blendPath = "";
-        private string outputFolder = "C:\\tmp\\";
+        private string blendPath;
+        private string outputFolder;
 
         // Single Settings
-        private int selectedFrame = 1; 
+        private int selectedFrame;
 
         // Animation Settings 
-        private int currentFrame = 1; 
-        private int startFrame = 1;
-        private int jumpFrame = 0;
-            // Render every x frames.
-        private int endFrame = 250;
+        private int currentFrame;
+        private int startFrame;
+        private int endFrame;
 
         // File Options
         private string[] RenderFormat = new string[22] { "TGA", "IRIS", "HAMX",
@@ -53,31 +51,69 @@ namespace FastF12
 
         public BlenderJob()
         {
-            //Should actually instantiate the class vars here.  
+            // Basic Settings
+            this.ProjectName = "Untitled";
+            this.RenderType = RenderType.Single;
+
+            this.blendPath = "";
+            this.outputFolder = "C:\\tmp\\";
+
+             // Single Settings
+            this.selectedFrame = 1; 
+
+            // Animation Settings 
+            this.currentFrame = 1; 
+            this.startFrame = 1;
+            this.endFrame = 250;
+
+              // File Options
+            this.fileExt = this.RenderFormat[3]; //JPEG
+            this.fileExtinName = false;
+            this.fileNaming = "####";
+
+            // Thread and Idle Settings
+            this.idleRendering = false;
+            this.threadsEnabled = false;
+            this.threadCount = 0;
+
+            // Misc Settings
+            this.debugging = false;
         }
 
         public String Run()
         {
+            // Usage: blender [-b <dir><file> [-o <dir><file> ][-F <format>][-x [0|1] ]
+            // [-t <threads>][-S <name>][-f <frame>][-s<frame> -e <frame> -a]]
+
+            // Run in background command
             string str = " -b ";
+            // Add Directory + File
+            str += this.blendPath;
+
+            // Add Output Directory 
+            str += " -o " + outputFolder;
+            // Add File Naming and Type
+            str += " -F " + this.fileExt + this.fileNaming;
+            str += " -x " + this.fileExtinName;
+
+            //Add Threads(If Requested)
+            if (this.threadsEnabled)
+                str += " -t " + this.threadCount ;
+
+            // Add Scene 
+            if(this.sceneName != null)
+                str += " -S " + this.sceneName;
+
+            // Add Single or Animation
             if (this.RenderType == RenderType.Single)
-                str += RenderSingle();
+                str += " -f " + this.selectedFrame;
             else if (this.RenderType == RenderType.Animation)
-                str += RenderAnimation();
+                str += " -s " + this.startFrame + " -e " + this.endFrame + " -a";
             else
-                str += "";
+                throw new System.InvalidOperationException("Invalid RenderType");
+
+            // Return command line arguments
             return str;
-        }
-
-        private String RenderSingle()
-        {
-            // TODO
-            return "";
-        }
-
-        private String RenderAnimation()
-        {
-            // TODO
-            return "";
         }
     }
 }
