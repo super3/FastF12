d26378ea5ad248a47f6fce4ca018c2843adf0bf6
Shawn Wilkinson <super3boy@gmail.com>
2011-10-14 19:42:02 -0400

    Copied over new job wizard

--- current/FastF12/FastF12/Program.cs
+++ current/FastF12/FastF12/Program.cs
@@ -15,7 +15,7 @@ namespace FastF12
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
