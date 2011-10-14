d918535d20759159dc5efb147ad73223981c593f
Shawn Wilkinson <super3boy@gmail.com>
2011-10-14 19:46:16 -0400

    renamed form1.cs to main.cs

---
+++ current/FastF12/FastF12/Main.Designer.cs
@@ -0,0 +1,147 @@
+﻿// Icons from "Jonas Rask Design Icons for Developers" Set
+
+namespace FastF12
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.newBtn = new System.Windows.Forms.Button();
+            this.editBtn = new System.Windows.Forms.Button();
+            this.trashBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.settingsBtn = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // newBtn
+            // 
+            this.newBtn.Image = global::FastF12.Properties.Resources._new;
+            this.newBtn.Location = new System.Drawing.Point(437, 12);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(45, 45);
+            this.newBtn.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.newBtn, "New");
+            this.newBtn.UseVisualStyleBackColor = true;
+            this.newBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // editBtn
+            // 
+            this.editBtn.Image = global::FastF12.Properties.Resources.edit;
+            this.editBtn.Location = new System.Drawing.Point(437, 63);
+            this.editBtn.Name = "editBtn";
+            this.editBtn.Size = new System.Drawing.Size(45, 45);
+            this.editBtn.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.editBtn, "Edit");
+            this.editBtn.UseVisualStyleBackColor = true;
+            // 
+            // trashBtn
+            // 
+            this.trashBtn.Image = global::FastF12.Properties.Resources.delete;
+            this.trashBtn.Location = new System.Drawing.Point(437, 114);
+            this.trashBtn.Name = "trashBtn";
+            this.trashBtn.Size = new System.Drawing.Size(45, 45);
+            this.trashBtn.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.trashBtn, "Trash");
+            this.trashBtn.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(419, 225);
+            this.listBox1.TabIndex = 3;
+            // 
+            // settingsBtn
+            // 
+            this.settingsBtn.Image = global::FastF12.Properties.Resources.settings;
+            this.settingsBtn.Location = new System.Drawing.Point(437, 192);
+            this.settingsBtn.Name = "settingsBtn";
+            this.settingsBtn.Size = new System.Drawing.Size(45, 45);
+            this.settingsBtn.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.settingsBtn, "Settings");
+            this.settingsBtn.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 250);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(494, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 272);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.settingsBtn);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trashBtn);
+            this.Controls.Add(this.editBtn);
+            this.Controls.Add(this.newBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "FastF12";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.Button editBtn;
+        private System.Windows.Forms.Button trashBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button settingsBtn;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ current/FastF12/FastF12/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FastF12
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Open Next Form and Keep Current Location
+            Wizard_Start wizard2 = new Wizard_Start();
+            wizard2.Show();       
+        }
+    }
+}
