0583ddea17f67f8637fab2a405e51149019f1ed6
Shawn Wilkinson <super3boy@gmail.com>
2011-10-20 01:25:14 -0400

    main.cs is now nice and pretty

--- current/FastF12/FastF12/Main.cs
+++ current/FastF12/FastF12/Main.cs
@@ -21,11 +21,32 @@ namespace FastF12
             InitializeComponent();
         }
 
-        // Launches Wizard_Start.cs and gets back completed object.
+        // Launches Wizard_Start.cs and Sets Completed Object to Reference.
         private void start_wizard(ref BlenderJob tmpBlendJob)
         {
             // Open Wizard 
             Wizard_Start wizard = new Wizard_Start(ref tmpBlendJob); // Pass by reference. Yay!
+
+            // Show wizard as a modal dialog and determine if DialogResult = OK.
+            if (wizard.ShowDialog(this) == DialogResult.OK)
+            {
+                // Get Back Passed Object
+                tmpBlendJob = wizard.returnJob;
+            }
+            else
+            {
+                // It was cancelled. 
+            }
+
+            // Clean-up and Return Location
+            wizard.Dispose();
+        }
+
+        // Launches Wizard_End.cs and Sets Completed Object to Reference.
+        private void end_wizard(ref BlenderJob tmpBlendJob)
+        {
+            // Open Wizard 
+            Wizard_End wizard = new Wizard_End(ref tmpBlendJob); // Pass by reference. Yay!
 
             // Show wizard as a modal dialog and determine if DialogResult = OK.
             if (wizard.ShowDialog(this) == DialogResult.OK)
@@ -49,6 +70,7 @@ namespace FastF12
 
             // Launch Wizard_Start and Return Filled Object
             start_wizard(ref tmpBlend);
+            end_wizard(ref tmpBlend);
 
             // Add to GUI and Loaded BlenderJobs
             allJobs.Add(tmpBlend);
